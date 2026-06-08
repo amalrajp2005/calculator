d64b00c45374d6e6e946ff34bdb261d93bb3dc68
Amalraj P <amalrajp2005@gmail.com>
2026-06-08 21:03:23 +0530

    Simple Calculator

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import { useState } from "react"
+const App = () => {
+
+  const [input, setInput] = useState("")
+ //handle click
+  const handleClick = (value) => {
+    setInput((prev) => prev + value);
+  };
+  //handleclear
+  const handleClear = () => {
+    setInput('');
+  };
+//handleDelete
+  const handleDelete = () => {
+    setInput((prev) => prev.slice(0, -1));
+  };
+  //handleEqual
+  const handleEqual = () => {
+    try {
+      setInput(eval(input).toString());
+    } catch (error) {
+      setInput('Error');
+    }
+  };
+  
+
+  return (
+    
+    <div className="calculator-container">
+      <h1>Simple Calculator</h1>
+      <div className="calculator">
+        <div className="display">{input || "0"}</div>
+        <div className="buttons">
+          <button className="btn-control" onClick={handleClear}>
+            C
+          </button>
+          <button className="btn-control" onClick={handleDelete}>
+            DEL
+          </button>
+          <button className="btn-operator">%</button>
+          <button className="btn-operator">*</button>
+         
+          <button className="btn" onClick={() => handleClick('7')}>
+            7
+          </button>
+          <button className="btn" onClick={() =>handleClick('8')}>8</button>
+           <button className="btn" onClick={() => handleClick('9')}>
+            9
+          </button>
+          <button className="btn-operator" onClick={() => handleClick('/')}>
+            /
+          </button>
+
+          <button className="btn" onClick={() => handleClick('4')}>
+            4
+          </button>
+          <button className="btn" onClick={() => handleClick('5')}>
+            5
+          </button>
+          <button className="btn" onClick={() => handleClick('6')}>
+            6
+          </button>
+          <button className="btn-operator" onClick={() => handleClick('+')}>
+            +
+          </button>
+
+          <button className="btn" onClick={() => handleClick('1')}>
+            1
+          </button>
+          <button className="btn" onClick={() => handleClick('2')}>
+            2
+          </button>
+          <button className="btn" onClick={() => handleClick('3')}>
+            3
+          </button>
+          <button className="btn-operator" onClick={() => handleClick('-')}>
+            -
+          </button>
+
+          <button className="btn zero" onClick={() => handleClick('0')}>
+            0
+          </button>
+          <button className="btn"onClick={()=>handleClick('.')}>.</button>
+          <button className="btn-equal" onClick={handleEqual}>
+            =
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
